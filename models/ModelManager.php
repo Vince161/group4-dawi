d1372f86eb4549233ef8a4e45e1bbb9d2a145229
Vince161 <65224100+Vince161@users.noreply.github.com>
2020-06-09 13:46:08 +0200

    Update ModelManager.php

--- models/ModelManager.php
+++ models/ModelManager.php
@@ -5,7 +5,7 @@ class ModelManager extends Model
 	public function getModManager()
 	{
 		 $this->getBdd();
-		 return $this->getAll('bdd', 'Etudiant');
+		 return $this->getAll('users', 'User');
 	}
 }
 
