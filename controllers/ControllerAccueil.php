733c16b86bb2477139072dbe7f6398a27e9df416
Vince161 <65224100+Vince161@users.noreply.github.com>
2020-06-09 13:48:53 +0200

    Update ControllerAccueil.php

--- controllers/ControllerAccueil.php
+++ controllers/ControllerAccueil.php
@@ -1,3 +1,30 @@
 <?php
 
->
+require_once('views/View.php');
+
+class ControllerAccueil
+{
+	private $_modelManager;
+	private $_view;
+
+	public function __construct($url)
+	{
+		if(isset($url) && count($url) > 1)
+			throw new Exception('Error Processing Request - Page Introuvable');
+		else 
+			$this->users();
+			
+	}
+
+	private function users()
+	{
+		$this->_modelManager = new ModelManager;
+		$users = $this->_modelManager->getModManager();
+
+		$this->_view = new View('Accueil');
+		$this->_view->generate(array('users' => $users));
+		
+	}
+}
+
+?>
