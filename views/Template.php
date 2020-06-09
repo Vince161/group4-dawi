239cd7248124991dd953992801eaed78e3c968c7
Vince161 <65224100+Vince161@users.noreply.github.com>
2020-06-09 13:53:48 +0200

    Create Template.php

---
+++ views/Template.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html lang="fr">
+ 		<head>
+   			<meta charset="utf-8" />
+ 			<title>Dawii for beginner</title>
+  		</head>
+  		<header>
+  			<h1><a href="<?= URL ?>">DAWIII</a></h1>
+  			<p>Bienvenue sur le site des dawiwiii</p>
+  		</header>
+  		<?= $content ?>
+  		<footer>
+  			<p>Créee par l'équipe DAWIII</p>
+  		</footer>
+  		<body>
+  		</body>
+ 		 
+
+</html>
